78de80175f9293933118546a3cba91bbabb7abfd
Felipe de Paula Santos <felipedpaulasantos@gmail.com>
2020-06-12 16:59:16 -0300

    StyleService para toolbar

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -3,6 +3,7 @@ export const loremIpsumPlaceHolder = "Lorem ipsum dolor sit amet, consectetur ad
 
 export interface ColorTheme {
 	name: string;
+	textColor: string;
 	lightest: boolean;
 	light: boolean;
 	dark: boolean;
@@ -10,17 +11,26 @@ export interface ColorTheme {
 }
 
 export class BootstrapTheme {
-  static readonly PRIMARY  = new BootstrapTheme('PRIMARY', { name: "primary", lightest: false, light: true, dark: true, darkest: false });
+  static readonly PRIMARY  = new BootstrapTheme(
+		'PRIMARY', { name: "primary", textColor: "light", lightest: false, light: true, dark: true, darkest: false });
 	static readonly SECONDARY  = new BootstrapTheme(
-		'SECONDARY', { name: "secondary", lightest: false, light: true, dark: true, darkest: false });
-	static readonly SUCCESS  = new BootstrapTheme('SUCCESS', { name: "success", lightest: false, light: true, dark: true, darkest: false });
-	static readonly DANGER  = new BootstrapTheme('DANGER', { name: "danger", lightest: false, light: true, dark: true, darkest: false });
-	static readonly INFO  = new BootstrapTheme('INFO', { name: "info", lightest: false, light: true, dark: true, darkest: false });
-	static readonly WARNING  = new BootstrapTheme('WARNING', { name: "warning", lightest: false, light: true, dark: true, darkest: false });
-	static readonly APOIO  = new BootstrapTheme('APOIO', { name: "apoio", lightest: false, light: true, dark: true, darkest: false });
-	static readonly AUX  = new BootstrapTheme('AUX', { name: "aux", lightest: false, light: true, dark: true, darkest: false });
-	static readonly LIGHT  = new BootstrapTheme('light', { name: "light", lightest: true, light: false, dark: true, darkest: true });
-	static readonly DARK  = new BootstrapTheme('DARK', { name: "dark", lightest: false, light: true, dark: false, darkest: true });
+		'SECONDARY', { name: "secondary", textColor: "light", lightest: false, light: true, dark: true, darkest: false });
+	static readonly SUCCESS  = new BootstrapTheme(
+		'SUCCESS', { name: "success", textColor: "light", lightest: false, light: true, dark: true, darkest: false });
+	static readonly DANGER  = new BootstrapTheme(
+		'DANGER', { name: "danger", textColor: "light", lightest: false, light: true, dark: true, darkest: false });
+	static readonly INFO  = new BootstrapTheme(
+		'INFO', { name: "info", textColor: "light", lightest: false, light: true, dark: true, darkest: false });
+	static readonly WARNING  = new BootstrapTheme(
+		'WARNING', { name: "warning", textColor: "light", lightest: false, light: true, dark: true, darkest: false });
+	static readonly APOIO  = new BootstrapTheme(
+		'APOIO', { name: "apoio", textColor: "light", lightest: false, light: true, dark: true, darkest: false });
+	static readonly AUX  = new BootstrapTheme(
+		'AUX', { name: "aux", textColor: "light", lightest: false, light: true, dark: true, darkest: false });
+	static readonly LIGHT  = new BootstrapTheme(
+		'LIGHT', { name: "light", textColor: "dark", lightest: true, light: false, dark: true, darkest: true });
+	static readonly DARK  = new BootstrapTheme(
+		'DARK', { name: "dark", textColor: "light", lightest: false, light: true, dark: false, darkest: true });
 
   // private to disallow creating other instances of this type
   private constructor(private readonly key: string, public readonly value: ColorTheme) {
@@ -30,3 +40,6 @@ export class BootstrapTheme {
     return this.key;
   }
 }
+
+export const LOGO_CAIXA_BRANCO_SRC = "assets/images/caixa-logo-x-branco.png";
+export const LOGO_CAIXA_SRC = "assets/images/caixa-logo-x.png";
--- src/app/shared/services/style.service.ts
+++ src/app/shared/services/style.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { LOGO_CAIXA_BRANCO_SRC } from '../constants/constants';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,25 @@ export class StyleService {
 
   constructor() { }
 
-  private toolbarBgSource = new BehaviorSubject<string>("bg-primary");
+  private toolbarBgSource = new BehaviorSubject<string>("primary");
   toolbarBg$ = this.toolbarBgSource.asObservable();
+
+  private toolbarTextSource = new BehaviorSubject<string>("light");
+  toolbarText$ = this.toolbarTextSource.asObservable();
+
+  private toolbarIconSource = new BehaviorSubject<string>(LOGO_CAIXA_BRANCO_SRC);
+  toolbarIcon$ = this.toolbarIconSource.asObservable();
 
   setToolbarBg(className: string) {
     this.toolbarBgSource.next(className);
+  }
+
+  setToolbarText(className: string) {
+    this.toolbarTextSource.next(className);
+  }
+
+  setToolbarIcon(src: string) {
+    this.toolbarIconSource.next(src);
   }
 
 }
