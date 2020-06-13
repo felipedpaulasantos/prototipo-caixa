f1ba40df9d02c5de173b56d48134c66346d3e0ab
Felipe de Paula Santos <felipedpaulasantos@gmail.com>
2020-06-13 02:57:49 -0300

    Atualização do Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -21,6 +21,12 @@ export class HomeComponent implements OnInit {
       description: "Consulte as cores disponíveis e a recomendação de seus usos"
     },
     {
+      name: 'Layout',
+      icon: 'fas fa-layer-group',
+      url: '/layout',
+      description: "Altera os temas da aplicação",
+    },
+    {
       name: "Conta",
       url: "/contas",
       icon: "fas fa-phone-alt",
