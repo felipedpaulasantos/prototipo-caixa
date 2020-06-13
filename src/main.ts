ee8c0ad94c2b1e43a450aa5f3720c0c4799b086e
Felipe de Paula Santos <felipedpaulasantos@gmail.com>
2020-06-13 19:43:25 -0300

    Correção main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+/* Ng Serve com AOT não funciona sem esse import */
+import '@angular/compiler';
+
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
