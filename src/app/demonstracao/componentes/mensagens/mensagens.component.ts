8dbc532869950c722fcd6672552b8cba75a2017a
Felipe de Paula Santos <felipedpaulasantos@gmail.com>
2020-06-15 19:54:13 -0300

    Mensagens - Modal form

--- src/app/demonstracao/componentes/mensagens/mensagens.component.ts
+++ src/app/demonstracao/componentes/mensagens/mensagens.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { ModalService } from 'src/app/shared/services/modal.service';
+import { ModalSize } from 'src/app/shared/components/modal/modal-options';
 
 @Component({
   selector: 'app-mensagens',
@@ -9,9 +11,12 @@ import { NgxSpinnerService } from 'ngx-spinner';
 })
 export class MensagensComponent implements OnInit {
 
+  modalSize = ModalSize;
+
   constructor(
     public toastr: ToastrService,
-    public spinner: NgxSpinnerService
+    public spinner: NgxSpinnerService,
+    private modalService: ModalService
   ) { }
 
   ngOnInit() {
@@ -22,6 +27,22 @@ export class MensagensComponent implements OnInit {
     setTimeout(() => {
       this.spinner.hide();
     }, 3000);
+  }
+
+  showTesteModal(titulo?: string, mensagem?: string, tamanho?: ModalSize, centro?: boolean) {
+    console.log(tamanho);
+    this.modalService.show({
+      showCancelar: true,
+      classTitulo: "",
+      titulo: titulo || "Título do modal",
+      mensagem: mensagem || "Mensagem do modal",
+      btOkTexto: "OK",
+      btnOkClass: "btn btn-caixa btn-secondary",
+      btnCancelarClass: "btn btn-caixa btn-info",
+      btCancelarTexto: "Cancelar",
+      tamanho: tamanho,
+      centralizado: centro
+    });
   }
 
 }
