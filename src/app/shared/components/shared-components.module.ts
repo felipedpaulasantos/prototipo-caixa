a383fae55bc61a664031ec425f74ac7fea0fa863
Felipe de Paula Santos <felipedpaulasantos@gmail.com>
2020-06-24 16:08:49 -0300

    Criação do code-view component

--- src/app/shared/components/shared-components.module.ts
+++ src/app/shared/components/shared-components.module.ts
@@ -1,21 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxSpinnerModule } from 'ngx-spinner';
-
+import { RouterModule } from '@angular/router';
+import { HighlightModule } from 'ngx-highlightjs';
 import { NgxSpinnerDefaultComponent } from './ngx-spinner-default/ngx-spinner-default.component';
-import { BootstrapSelectComponent } from './bootstrap-select/bootstrap-select.component';
-import { CustomChartComponent } from './charts/bar-chart/bar-chart.component';
 import { ChartsModule } from 'ng2-charts';
 import { NgxMaskModule } from 'ngx-mask';
+
+import { BootstrapSelectComponent } from './bootstrap-select/bootstrap-select.component';
+import { CustomChartComponent } from './charts/bar-chart/bar-chart.component';
 import { TooltipIconComponent } from './tooltip-icon/tooltip-icon.component';
 import { ModalComponent } from './modal/modal.component';
 import { BackButtonComponent } from './buttons/back-button/back-button.component';
 import { AccordionComponent } from './accordion/accordion.component';
-import { RouterModule } from '@angular/router';
 import { InputCaixaComponent } from './input-caixa/input-caixa.component';
 import { InputCaixaDirective } from './input-caixa/input-caixa.directive';
 import { SelectCaixaComponent } from './select-caixa/select-caixa.component';
 import { SelectCaixaDirective } from './select-caixa/select-caixa.directive';
+import { CodeViewComponent } from './code-view/code-view.component';
 
 @NgModule({
 	declarations: [
@@ -29,13 +31,15 @@ import { SelectCaixaDirective } from './select-caixa/select-caixa.directive';
 		InputCaixaComponent,
 		InputCaixaDirective,
 		SelectCaixaDirective,
-		SelectCaixaComponent
+		SelectCaixaComponent,
+		CodeViewComponent
 	],
 	imports: [
 		CommonModule,
 		NgxSpinnerModule,
 		ChartsModule,
 		RouterModule,
+		HighlightModule,
 		NgxMaskModule.forChild()
 	],
 	exports: [
@@ -49,7 +53,8 @@ import { SelectCaixaDirective } from './select-caixa/select-caixa.directive';
 		InputCaixaComponent,
 		InputCaixaDirective,
 		SelectCaixaDirective,
-		SelectCaixaComponent
+		SelectCaixaComponent,
+		CodeViewComponent
 	]
 })
 export class SharedComponentsModule { }
