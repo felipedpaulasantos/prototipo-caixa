1fcf37e9115c11939fbfe9176d62c2a07db58df2
Felipe <felipedpaulasantos@gmail.com>
2020-11-12 12:46:27 -0300

    Card Caixa Component

--- src/app/guia-caixa/components/card-caixa/card-caixa.component.ts
+++ src/app/guia-caixa/components/card-caixa/card-caixa.component.ts
@@ -18,15 +18,17 @@ export class CardCaixaComponent implements OnInit, AfterContentInit {
   cardElement: any;
 
   isFullscreen: boolean = false;
-  originalTopPosition: any;
-  originalLeftPosition: any;
-  originalHeight: any;
-  originalWidth: any;
+  originalTopPosition: string;
+  originalLeftPosition: string;
+
+  originalHeight: number;
+  originalMinHeight: string;
+  originalMaxHeight: string;
+  originalWidth: number;
+  originalMinWidth: string;
+  originalMaxWidth: string;
+
   wasMoved: boolean = false;
-  pos1 = 0;
-  pos2 = 0;
-  pos3 = 0;
-  pos4 = 0;
 
   constructor(
     element: ElementRef,
@@ -46,23 +48,24 @@ export class CardCaixaComponent implements OnInit, AfterContentInit {
   setOriginalPosition(): void {
     this.originalTopPosition = this.cardElement.style.top;
     this.originalLeftPosition = this.cardElement.style.left;
-
-    console.log(this.cardElement);
   }
 
   setOriginalSize(): void {
-    this.originalHeight = this.cardElement.offsetHeight + "px";
-    this.originalWidth = this.cardElement.offsetWidth + "px";
+    this.originalHeight = this.cardElement.offsetHeight;
+    this.originalWidth = this.cardElement.offsetWidth;
+  }
 
-    this.renderer.setStyle(this.cardElement, "minHeight", this.originalHeight);
-    this.renderer.setStyle(this.cardElement, "height", this.originalHeight);
-    this.renderer.setStyle(this.cardElement, "maxHeight", this.originalHeight);
+  preventResize(): void {
+    const stringHeight = this.originalHeight + "px";
+    const stringWidth = this.originalWidth + "px";
 
-    this.renderer.setStyle(this.cardElement, "minWidth", this.originalWidth);
-    this.renderer.setStyle(this.cardElement, "width", this.originalWidth);
-    this.renderer.setStyle(this.cardElement, "maxWidth", this.originalWidth);
+    this.renderer.setStyle(this.cardElement, "minHeight", stringHeight);
+    this.renderer.setStyle(this.cardElement, "height", stringHeight);
+    this.renderer.setStyle(this.cardElement, "maxHeight", stringHeight);
 
-    console.log("TESTE", this.cardElement.style.height);
+    this.renderer.setStyle(this.cardElement, "minWidth", stringWidth);
+    this.renderer.setStyle(this.cardElement, "width", stringWidth);
+    this.renderer.setStyle(this.cardElement, "maxWidth", stringWidth);
   }
 
   resetOriginalPosition(): void {
@@ -70,6 +73,29 @@ export class CardCaixaComponent implements OnInit, AfterContentInit {
     this.cardElement.style.top = this.originalTopPosition;
     this.cardElement.style.position = "relative";
     this.wasMoved = false;
+  }
+
+  resetOriginalSize(): void {
+
+    this.renderer.setStyle(this.cardElement, "minHeight", this.originalMinHeight || null);
+    this.renderer.setStyle(this.cardElement, "maxHeight", this.originalMaxHeight || null);
+    this.renderer.setStyle(this.cardElement, "height", this.originalHeight + "px");
+    this.renderer.setStyle(this.cardElement, "height", null);
+
+    this.renderer.setStyle(this.cardElement, "minWidth", this.originalMinWidth || null);
+    this.renderer.setStyle(this.cardElement, "maxWidth", this.originalMaxWidth || null);
+    this.renderer.setStyle(this.cardElement, "width", this.originalWidth + "px");
+    this.renderer.setStyle(this.cardElement, "width", null);
+  }
+
+  reset(): void {
+    this.resetOriginalPosition(); 
+    this.resetOriginalSize()
+  }
+
+  moved(): void {
+    this.wasMoved = true; 
+    this.preventResize(); 
   }
 
   toggleFullscreen(): void {
@@ -98,9 +124,9 @@ export class CardCaixaComponent implements OnInit, AfterContentInit {
   dragElement(): void {
 
     this.setOriginalPosition();
+    this.setOriginalSize();
 
     const elmnt = this.cardElement;
-    console.log("ELEMENT", this.cardElement);
 
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
     if (this.moveButton.nativeElement) {
@@ -143,48 +169,6 @@ export class CardCaixaComponent implements OnInit, AfterContentInit {
       document.onmouseup = null;
       document.onmousemove = null;
     }
-  }
-
-  dragElementeee(): void {
-    if (this.moveButton.nativeElement) {
-      // if present, the header is where you move the DIV from:
-      this.moveButton.nativeElement.onmousedown = this.dragMouseDown;
-    } else {
-      // otherwise, move the DIV from anywhere inside the DIV:
-      this.cardElement.onmousedown = this.dragMouseDown;
-    }
-  }
-
-  dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    this.pos3 = e.clientX;
-    this.pos4 = e.clientY;
-    document.onmouseup = this.closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = this.elementDrag;
-  }
-
-  elementDrag(e) {
-    this.cardElement.style.position = "absolute";
-    this.wasMoved = true;
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    this.pos1 = this.pos3 - e.clientX;
-    this.pos2 = this.pos4 - e.clientY;
-    this.pos3 = e.clientX;
-    this.pos4 = e.clientY;
-    // set the element's new position:
-    this.cardElement.style.top = (this.cardElement.offsetTop - this.pos2) + "px";
-    this.cardElement.style.left = (this.cardElement.offsetLeft - this.pos1) + "px";
-  }
-
-  closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
   }
 
 }
