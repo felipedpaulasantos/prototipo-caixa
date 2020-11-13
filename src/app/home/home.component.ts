cfbc6dfb491a575069ac46e4fa97b767bdf08f04
Felipe <felipedpaulasantos@gmail.com>
2020-11-13 12:49:24 -0300

    Demonstração de upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ComponentFactoryResolver, Injector, OnInit, ViewContainerRef } from "@angular/core";
+import { Component, ComponentFactoryResolver, ElementRef, Injector, OnInit, Renderer2, ViewContainerRef } from "@angular/core";
 import { FormBuilder, Validators } from '@angular/forms';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
@@ -28,7 +28,8 @@ export class HomeComponent implements OnInit {
     private toastr: ToastrService,
     private modal: ModalService,
     private resolver: ComponentFactoryResolver,
-    private injector: Injector
+    private injector: Injector,
+    private renderer: Renderer2
   ) {}
 
   rows: any[] = [];
@@ -42,9 +43,17 @@ export class HomeComponent implements OnInit {
   });
   cliente = null;
 
+  previaSrc: string;
+  uploadedFile: File = null;
+
   ngOnInit() {
     this.rows = this.groupColumns(this.resources);
     this.clientePesquisado();
+    this.populaContratos();
+    this.preventDragDropDefault();
+  }
+
+  populaContratos(): void {
     for (let index = 0; index < 10; index++) {
       const contrato = {
         numero: `4200.160.0150${index}-${index}`,
@@ -101,4 +110,78 @@ export class HomeComponent implements OnInit {
       titulo: "Detalhamento"
     });
   }
+
+  drop(ev: DragEvent, cardEl: ElementRef) {
+    ev.preventDefault();
+    this.previaSrc = null;
+    this.uploadedFile = null;
+    if (ev.dataTransfer && ev.dataTransfer.files && ev.dataTransfer.files[0]) {
+      const files = ev.dataTransfer.files;
+      this.uploadArquivo(files[0]);
+    }
+    this.dragLeave(null, cardEl);
+  }
+
+  dragOver(ev: any, cardEl: ElementRef) {
+    this.renderer.addClass(cardEl, "drag-hover");
+  }
+
+  dragLeave(ev: any, cardEl: ElementRef) {
+    this.renderer.removeClass(cardEl, "drag-hover");
+  }
+
+  uploadArquivo(file: File) {
+    this.spinner.show("spinnerUpload");
+    setTimeout(() => {
+      this.fromFileToBase64(file).then(
+        (base64) => {
+          this.previaSrc = `data:${file.type};base64,${base64}`;
+          this.uploadedFile = file;
+          this.spinner.hide("spinnerUpload");
+        }
+      )
+    }, 2000);
+  }
+
+  addDocumento() {
+    this.toastr.success("Documento adicionado com sucesso!");
+  }
+
+  fromFileToBase64(file: File, comMetadata: boolean = false): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+
+        /* Metadata: 'data:{mimetype};base64,' */
+        const resultComMetadata = reader.result.toString();
+        const resultSemMetadata = resultComMetadata.replace(/^data:(.*,)?/, "");
+
+        let base64 = comMetadata ? resultComMetadata : resultSemMetadata;
+
+        /* Preenche o base64 com '=' caso não seja múltiplo de 4 - correção de padding */
+        if ((base64.length % 4) > 0) {
+          base64 += "=".repeat(4 - (resultSemMetadata.length % 4));
+        }
+        resolve(base64);
+      };
+      reader.onerror = error => reject(error);
+    });
+  }
+
+  open(src: string) {
+    window.open(src);
+  }
+
+  preventDragDropDefault() {
+    window.addEventListener("dragover", function(e: any) {
+      e = e || event;
+      e.preventDefault();
+    }, false);
+    window.addEventListener("drop", function(e: any) {
+      e = e || event;
+      e.preventDefault();
+    }, false);
+  }
+
 }
