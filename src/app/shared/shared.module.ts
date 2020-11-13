b83a0bf32fc26334d51c42fbda71a1c43ae63afd
Felipe <felipedpaulasantos@gmail.com>
2020-11-13 12:48:58 -0300

    Bytesize Pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from "@angular/core";
 import { SharedComponentsModule } from "./components/shared-components.module";
 import { ScrollSpyDirective } from './directives/scrollspy.directive';
+import { ByteSizePipe } from './pipes/bytesize-pipe';
 
 @NgModule({
   imports: [SharedComponentsModule],
-  declarations: [ScrollSpyDirective],
-  exports: [SharedComponentsModule, ScrollSpyDirective]
+  declarations: [ScrollSpyDirective, ByteSizePipe],
+  exports: [SharedComponentsModule, ScrollSpyDirective, ByteSizePipe]
 })
 
 export class SharedModule {}
