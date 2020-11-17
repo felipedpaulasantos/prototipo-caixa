0630c9b3165291b8d7da9fb782ab5a9d41182a8a
Felipe <felipedpaulasantos@gmail.com>
2020-11-17 14:30:10 -0300

    CPF Pipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from "@angular/core";
 import { SharedComponentsModule } from "./components/shared-components.module";
 import { ScrollSpyDirective } from './directives/scrollspy.directive';
 import { ByteSizePipe } from './pipes/bytesize-pipe';
+import { CpfPipe } from './pipes/cpf-pipe';
 
 @NgModule({
   imports: [SharedComponentsModule],
-  declarations: [ScrollSpyDirective, ByteSizePipe],
-  exports: [SharedComponentsModule, ScrollSpyDirective, ByteSizePipe]
+  declarations: [ScrollSpyDirective, ByteSizePipe, CpfPipe],
+  exports: [SharedComponentsModule, ScrollSpyDirective, ByteSizePipe, CpfPipe]
 })
 
 export class SharedModule {}
