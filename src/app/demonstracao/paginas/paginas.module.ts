6e4e5cfd43d464224fea61589e47bfb6aed37521
Felipe <felipedpaulasantos@gmail.com>
2020-11-17 14:38:09 -0300

    Páginas - upload

--- src/app/demonstracao/paginas/paginas.module.ts
+++ src/app/demonstracao/paginas/paginas.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PaginasRoutingModule } from './paginas-routing.module';
-import { PaginasComponent } from './paginas.component';
-import { SharedModule } from 'src/app/shared/shared.module';
-import { GuiaCaixaModule } from 'src/app/guia-caixa/guia-caixa.module';
-import { UploadComponent } from './upload/upload.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+import { PaginasComponent } from './paginas.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { NgxMaskModule } from 'ngx-mask';
+import { GuiaCaixaModule } from 'src/app/guia-caixa/guia-caixa.module';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [PaginasComponent, UploadComponent],
@@ -17,7 +18,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     SharedModule,
     GuiaCaixaModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxMaskModule
   ]
 })
 export class PaginasModule { }
