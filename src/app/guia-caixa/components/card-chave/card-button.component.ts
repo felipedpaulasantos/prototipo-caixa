9efb2497f2fd56f07a7e0af3bdd02a9f78681f24
Felipe <felipedpaulasantos@gmail.com>
2020-12-07 20:14:08 -0300

    Card-button

--- src/app/guia-caixa/components/card-chave/card-button.component.ts
+++ src/app/guia-caixa/components/card-chave/card-button.component.ts
@@ -1,6 +1,7 @@
-import {Component, Input, OnInit, OnChanges, ElementRef, ChangeDetectionStrategy} from "@angular/core";
+import { Component, Input, OnInit, OnChanges, ElementRef, ChangeDetectionStrategy, ContentChild, TemplateRef } from "@angular/core";
 import { FormBuilder } from "@angular/forms";
 import { FormGroup } from "@angular/forms";
+
 import { CardChave } from './listar-chaves';
 
 @Component({
@@ -9,12 +10,20 @@ import { CardChave } from './listar-chaves';
   styleUrls: ["./card-button.component.css"],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CardButtonComponent  implements OnInit, OnChanges {
+export class CardButtonComponent implements OnInit, OnChanges {
+
+  @ContentChild("directive", { read: TemplateRef })
+  template: TemplateRef<any>;
+
+  @Input()
+  isChecked = false;
 
   @Input()
   card: CardChave;
+
   @Input()
   index: any;
+
   formCard: FormGroup;
   valor: any;
 
@@ -28,67 +37,71 @@ export class CardButtonComponent  implements OnInit, OnChanges {
   ICON_CELULAR = "fas fa-mobile-alt";
   ICON_EMAIL = "fas fa-envelope";
 
-    constructor(
-      private fb: FormBuilder    ) {
-/*       pixService.changeEmittedAutenticarCliente$.subscribe((card: CardChave) => this.atualizarValorCard()); */
-    }
+  constructor(
+    private fb: FormBuilder) {
+    /*       pixService.changeEmittedAutenticarCliente$.subscribe((card: CardChave) => this.atualizarValorCard()); */
+  }
 
-    ngOnInit(): void {
-
-    }
-
-    ngOnChanges(): void {
-      this.formCard = this.fb.group({
-        selecionado: [this.card.selecionado],
-        valor: [this.card.valor]
-      });
-
-/*       if (this.card) {
-        switch (this.card.tipo) {
-          case TipoChaveCard.DOCUMENTO:
-          case TipoChave.CPF:
-          case TipoChave.CNPJ:
-            this.card.icone = this.ICON_DOCUMENTO
-            this.card.titulo = this.TITLE_DOCUMENTO
-            break;
-          case TipoChaveCard.ALEATORIA:
-            this.card.icone = this.ICON_ALEATORIA;
-            this.card.titulo = this.TITLE_ALEATORIA
-            break;
-          case TipoChaveCard.CELULAR:
-          case TipoChave.PHONE:
-            this.card.icone = this.ICON_CELULAR;
-            this.card.titulo = this.TITLE_CELULAR
-            break;
-          case TipoChaveCard.EMAIL:
-            this.card.icone = this.ICON_EMAIL;
-            this.card.titulo = this.TITLE_EMAIL
-            break;
-          default:
-            break;
-        }
-      } */
-
-      this.formCard.get("valor").valueChanges.subscribe(() => {
-        this.atualizarValorCard();
-      });
-    }
-
-    atualizarListaTasks() {
-/*       this.pixService.nextChangeCardSelecionado(this.card); */
-      this.atualizarValorCard();
-    }
-
-    atualizarValorCard() {
-        if (this.card.selecionado) {
-          this.card.valor = this.formCard.get("valor").value;
-/*           this.pixService.nextChangeCardValor(this.card); */
-        }
-    }
-
-    nextStep() {
-
-    }
-
+  ngOnInit(): void {
 
   }
+
+  ngOnChanges(): void {
+    this.formCard = this.fb.group({
+      selecionado: [this.card.selecionado],
+      valor: [this.card.valor]
+    });
+
+    /*       if (this.card) {
+            switch (this.card.tipo) {
+              case TipoChaveCard.DOCUMENTO:
+              case TipoChave.CPF:
+              case TipoChave.CNPJ:
+                this.card.icone = this.ICON_DOCUMENTO
+                this.card.titulo = this.TITLE_DOCUMENTO
+                break;
+              case TipoChaveCard.ALEATORIA:
+                this.card.icone = this.ICON_ALEATORIA;
+                this.card.titulo = this.TITLE_ALEATORIA
+                break;
+              case TipoChaveCard.CELULAR:
+              case TipoChave.PHONE:
+                this.card.icone = this.ICON_CELULAR;
+                this.card.titulo = this.TITLE_CELULAR
+                break;
+              case TipoChaveCard.EMAIL:
+                this.card.icone = this.ICON_EMAIL;
+                this.card.titulo = this.TITLE_EMAIL
+                break;
+              default:
+                break;
+            }
+          } */
+
+    this.formCard.get("valor").valueChanges.subscribe(() => {
+      this.atualizarValorCard();
+    });
+  }
+
+  atualizarListaTasks() {
+    /*       this.pixService.nextChangeCardSelecionado(this.card); */
+    this.atualizarValorCard();
+  }
+
+  atualizarValorCard() {
+    if (this.card.selecionado) {
+      this.card.valor = this.formCard.get("valor").value;
+      /*           this.pixService.nextChangeCardValor(this.card); */
+    }
+  }
+
+  nextStep() {
+
+  }
+
+  updateChecked(ev: any) {
+    console.log("TESTE", ev);
+    this.isChecked = !this.isChecked;
+  }
+
+}
