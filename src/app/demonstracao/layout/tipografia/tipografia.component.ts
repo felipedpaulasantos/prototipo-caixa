c7dcf903c578681f35bd6a50720f8f57cb67139c
Felipe <felipedpaulasantos@gmail.com>
2021-03-15 15:15:40 -0300

    Tipografia - fonte

--- src/app/demonstracao/layout/tipografia/tipografia.component.ts
+++ src/app/demonstracao/layout/tipografia/tipografia.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
 import { loremIpsumPlaceHolder } from "src/app/guia-caixa/constants/constants";
-
+import { CodeFixedNavItem } from "src/app/shared/components/code-fixed-nav/code-fixed-nav.component";
+import { ComponentesInterface } from "../../componentes/componentes-interface";
 
 
 @Component({
   selector: "app-tipografia",
   templateUrl: "./tipografia.component.html",
-  styleUrls: ["./tipografia.component.scss"]
+  styleUrls: ["./tipografia.component.scss"],
+  host: { "(window:scroll)": "onScroll($event)" }
 })
-export class TipografiaComponent implements OnInit {
+export class TipografiaComponent extends ComponentesInterface implements OnInit {
 
-  constructor() { }
+  constructor(public toastr: ToastrService) {
+    super(toastr);
+  }
+
+  @ViewChild("scrollElement") scrollElement;
+
+  sectionOffset = 0;
+  spiedTags = ["APP-DOCUMENTACAO-TEMPLATE"];
+  currentSection = "painelCardFonte";
+
+  navItems: CodeFixedNavItem[] = [
+    { id: "painelCardFonte", name: "Fonte" },
+    { id: "painelCardTitulosProprios", name: "Títulos Próprios" },
+    { id: "painelCardTamanhoPadrao", name: "Títulos - tamanho padrão" },
+    { id: "painelCardIcones", name: "Ícones" }
+  ];
 
   placeholder = loremIpsumPlaceHolder;
 
@@ -27,20 +45,28 @@ export class TipografiaComponent implements OnInit {
     {
       tag: "<h1 class='titulo'></h1>",
       exemplo: "<h1 class='titulo'>Título de página</h1>",
-      uso: `Sempre com a tag h1, no início de cada página. <br>Aplica a cor de destaque e acrescenta espaçamento inferior de 2rem`
+      uso: `Sempre com a tag h1, no início de cada página. <br>Aplica a fonte Futura LT Bold, a cor de destaque e acrescenta espaçamento inferior de 2rem`
     },
     {
       tag: "<h5 class='subtitulo'></h5>",
       exemplo: "<h5 class='subtitulo'>Subtítulo padrão</h5>",
-      uso: `Pode ser utilizada nas tags h2 até h6. <br>Aplica a fonte em negrito e acrescenta espaçamento inferior de 1rem.`
+      uso: `Pode ser utilizada nas tags h2 até h6. <br>Aplica a fonte Futura LT Bold e acrescenta espaçamento inferior de 1rem.`
     },
   ];
 
+  fontIntroducao = `<span>Texto na fonte padrão.</span>
+<br>
+<b>Texto com a tag "b".</b>
+<br>
+<strong>Texto com a tag "strong".</strong>
+<br>
+<span class="font-fam-bold">Texto com a classe "font-fam-bold".</span>`.trim();
+
   iconesExemplo = `<span class="mr-5">
-  <i class="fa fa-check mr-3"></i>
-  <i class="fa fa-times mr-3"></i>
-  <i class="fa fa-exclamation mr-3"></i>
-  <i class="fa fa-info mr-3"></i>
+  <i class="fa fa-check mr-3"></i> <!-- Sucesso -->
+  <i class="fa fa-times mr-3"></i> <!-- Erro -->
+  <i class="fa fa-exclamation mr-3"></i> <!-- Alerta -->
+  <i class="fa fa-info mr-3"></i> <!-- Informação -->
 </span>
 <h4 class="d-inline mr-5">
   <i class="fa fa-check fa-lg text-success mr-3"></i>
