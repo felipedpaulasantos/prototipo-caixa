1992f992321d33e6957c5d3b1c0ebd5a93962375
Felipe <felipedpaulasantos@gmail.com>
2021-06-08 22:29:26 -0300

    Stepper demo - code examples

--- src/app/demonstracao/componentes/stepper-demonstracao/stepper-demonstracao.component.ts
+++ src/app/demonstracao/componentes/stepper-demonstracao/stepper-demonstracao.component.ts
@@ -6,10 +6,10 @@ import { CodeFixedNavItem } from "src/app/shared/components/code-fixed-nav/code-
 import { DocApiProperties } from "src/app/shared/components/documentacao-api-table/documentacao-api-table.component";
 import { ComponentesInterface } from "../componentes-interface";
 import { stepperApiProps } from "./stepper-api-props";
+import { StepperCodeExamples } from "./stepper-code-examples";
 
 @Component({
   templateUrl: "./stepper-demonstracao.component.html",
-  styleUrls: ["./stepper-demonstracao.component.scss"],
   host: { "(window:scroll)": "onScroll($event)" }
 })
 export class StepperDemonstracaoComponent extends ComponentesInterface implements OnInit {
@@ -30,212 +30,11 @@ export class StepperDemonstracaoComponent extends ComponentesInterface implement
     { id: "painelStepperInterno", name: "Conteúdo Interno" }
   ];
 
-  codeDescricao = `<cx-stepper 
-  [steps]="passos" 
-  [currentStep]="passoAtual"
-  (changeStep)="passoAtual = $event">
-</cx-stepper>`.trim();
-
-  codeTsDescricao = `  passos: StepperItem[] = [
-  { title: "Primeiro passo" },
-  { title: "Segundo passo" },
-  { title: "Terceiro passo" },
-  { title: "Quarto passo" },
-  { title: "Quinto passo" }
-];
-passoAtual = 0;`.trim();
+  examples = StepperCodeExamples;
 
   propsApi: DocApiProperties[] = stepperApiProps;
 
   showTabsStepperPadrao = false;
-  htmlCodeStepperPadrao = `<cx-stepper [steps]="passos" [currentStep]="passoAtual" (changeStep)="passoAtual = $event"></cx-stepper>
-
-<div *ngFor="let passo of passos; let i = index">
-  <ng-container *ngIf="i == passoAtual">
-    <h2 class="text-center">{{ passo }}</h2>
-
-    <button [disabled]="passoAtual == 0"
-    (click)="passoAtual = passoAtual - 1" class="btn btn-cancel mx-2">
-        Voltar
-    </button>
-
-    <button [disabled]="passoAtual == passos.length - 1"
-    (click)="passoAtual = passoAtual + 1" class="btn btn-accent mx-2">
-        Avançar
-    </button>
-  </ng-container>
-</div>`.trim();
-
-  tsCodeStepperPadrao = `  import { Component } from '@angular/core';
-
-  @Component({
-      selector: 'app-stepper',
-      templateUrl: './stepper.component.html',
-      styleUrls: ['./stepper.component.scss']
-  })
-  export class StepperComponent {
-
-    constructor() {}
-
-    passoAtual = 0;
-
-    passos = [
-      'Primeiro passo',
-      'Segundo passo',
-      'Terceiro passo',
-      'Quarto passo',
-      'Passo final'
-    ];
-  }
-  `.trimRight();
-
-  htmlCodeStepperGuiado = `<cx-stepper [freeNavigation]="false" [steps]="passos" [currentStep]="passoAtual" (changeStep)="passoAtual = $event"></cx-stepper>
-
-<div *ngFor="let passo of passos; let i = index">
-  <ng-container *ngIf="i == passoAtual">
-    <h2 class="text-center">{{ passo }}</h2>
-
-    <button [disabled]="passoAtual == 0"
-    (click)="passoAtual = passoAtual - 1" class="btn btn-cancel mx-2">
-        Voltar
-    </button>
-
-    <button [disabled]="passoAtual == passos.length - 1"
-    (click)="passoAtual = passoAtual + 1" class="btn btn-accent mx-2">
-        Avançar
-    </button>
-  </ng-container>
-</div>`.trim();
-
-  tsCodeStepperGuiado = `  import { Component } from '@angular/core';
-
-  @Component({
-      selector: 'app-stepper',
-      templateUrl: './stepper.component.html',
-      styleUrls: ['./stepper.component.scss']
-  })
-  export class StepperComponent {
-
-    constructor() {}
-
-    passoAtual = 0;
-
-    passos = [
-      'Primeiro passo',
-      'Segundo passo',
-      'Terceiro passo',
-      'Quarto passo',
-      'Passo final'
-    ];
-  }
-  `.trimRight();
-
-  htmlCodeStepperTemplate = `<cx-stepper [steps]="passos" #stepperExemplo (changeStep)="passoAtual = $event"></cx-stepper>
-
-  <div *ngFor="let passo of passos; let i = index">
-     <ng-container *ngIf="i == passoAtual">
-        <h2 class="text-center">{{ passo }}</h2>
-     </ng-container>
-  </div>
-
-  <button [disabled]="stepperExemplo.currentStep == 0"
-  (click)="stepperExemplo.first()" class="btn btn-aux mx-2">
-     Primeiro
-  </button>
-
-  <button [disabled]="stepperExemplo.currentStep == 0"
-  (click)="stepperExemplo.previous()" class="btn btn-cancel mx-2">
-     Voltar
-  </button>
-
-  <button [disabled]="stepperExemplo.currentStep == passos.length - 1"
-  (click)="stepperExemplo.next()" class="btn btn-accent mx-2">
-     Avançar
-  </button>`.trim();
-
-  tsCodeStepperTemplate = `  import { Component } from '@angular/core';
-
-  @Component({
-      selector: 'app-stepper',
-      templateUrl: './stepper.component.html',
-      styleUrls: ['./stepper.component.scss']
-  })
-  export class StepperComponent {
-
-    constructor() {}
-
-    passoAtual = 0;
-
-    passos = [
-      'Primeiro passo',
-      'Segundo passo',
-      'Terceiro passo',
-      'Quarto passo',
-      'Passo final'
-    ];
-  }
-  `.trimRight();
-
-  htmlCodeStepperInterno = `<cx-stepper [steps]="passos" #stepperInterno>
-  <div *cxStepper>
-     <h2 class="text-center">Primeiro conteúdo interno</h2>
-  </div>
-
-  <div *cxStepper>
-     <h2 class="text-center">Segundo conteúdo interno</h2>
-  </div>
-
-  <div *cxStepper>
-     <h2 class="text-center">Terceiro conteúdo interno</h2>
-  </div>
-
-  <div *cxStepper>
-     <h2 class="text-center">Quarto conteúdo interno</h2>
-  </div>
-
-  <div *cxStepper>
-     <h2 class="text-center">Quinto conteúdo interno</h2>
-  </div>
-</cx-stepper>
-
-<button [disabled]="stepperInterno.currentStep == 0"
-(click)="stepperInterno.first()" class="btn btn-aux mx-2">
-  Primeiro
-</button>
-
-<button [disabled]="stepperInterno.currentStep == 0"
-(click)="stepperInterno.previous()" class="btn btn-cancel mx-2">
-  Voltar
-</button>
-
-<button [disabled]="stepperInterno.currentStep == passos.length - 1"
-(click)="stepperInterno.next()" class="btn btn-accent mx-2">
-  Avançar
-</button>`.trim();
-
-  tsCodeStepperInterno = `  import { Component } from '@angular/core';
-
-  @Component({
-      selector: 'app-stepper',
-      templateUrl: './stepper.component.html',
-      styleUrls: ['./stepper.component.scss']
-  })
-  export class StepperComponent {
-
-    constructor() {}
-
-    passos = [
-      'Primeiro passo',
-      'Segundo passo',
-      'Terceiro passo',
-      'Quarto passo',
-      'Passo final'
-    ];
-
-  }
-  `.trimRight();
-
-
 
   passos: StepperItem[] = [
     { title: "Primeiro passo" },
@@ -260,8 +59,6 @@ passoAtual = 0;`.trim();
   completedIcon = null;
   freeNavigation = true;
   showCompletedMessage = true;
-
-  temas = BootstrapTheme.getTemas();
 
   ngOnInit(): void { }
 
